e080bb79ee9baf464abe03f421c705f40fa753af
Soo-Yeon Kim <99032687+hoijoii@users.noreply.github.com>
2022-05-27 00:00:12 +0900

    Update Facebook Interview : Rearrange string
    잘못된 파일 업로드 : 럭키 스트레이트 -> 문자열재정렬 수정

--- Implementation/implementation_Facebook_Interview_rearrange_string.py
+++ Implementation/implementation_Facebook_Interview_rearrange_string.py
@@ -1,12 +1,15 @@
 import sys
-data = list(sys.stdin.readline().rstrip())
-intdata = list(map(int, data)) # 문자열의 str 요소들을 int 자료형으로 바꿈.
-l = len(data)
+data = list(sys.stdin.readline().rstrip()) # 문자열 입력받음
 
-left = intdata[0:l//2]
-right = intdata[l//2:]
+data.sort() # 알파벳과 숫자 오름차순 정렬. 숫자가 먼저 정렬됨.
+result = list() # 결과를 저장할 리스트
+sum = 0
 
-if sum(left) == sum(right):
-    print("LUCKY")
-else:
-    print("READY")
+for i in data:
+    if i.isdigit(): # 숫자면 참을 반환하는 함수
+        sum += int(i) # 만약 요소가 숫자라면 더함
+    else:
+        result.append(i) # 숫자가 아니라면 리스트에 추가
+result.append(str(sum)) # 더해진 숫자가 int 이므로 str 로 바꿔줌
+
+print(''.join(result)) # 문자열을 ''와 공백 제외하고 붙여서 출력
