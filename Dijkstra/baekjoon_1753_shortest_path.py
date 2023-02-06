602f117650bc04756f185aeaf2b011c99fbd282d
hoijoii <oodh3328@gmail.com>
2023-02-06 14:07:10 +0900

    최단경로

---
+++ Dijkstra/baekjoon_1753_shortest_path.py
@@ -0,0 +1,37 @@
+import heapq
+
+ve = list(map(int, input().split())) # V, E
+start = int(input())
+uvw = [list(map(int, input().split())) for _ in range(ve[1])] # u, v, w
+
+INF = 1000000;
+
+costs = [[INF for _ in range(ve[0])] for _ in range(ve[0])] # 비용 테이블 (행:출발노드, 열:도착노드, 값:비용)
+for u, v, w in uvw:
+    costs[u-1][v-1] = w
+
+"""priority queue 힙 구조 사용할 것"""
+
+def calculate_distances(graph, starting):
+    distances = {vertex: float('inf') for vertex in graph}
+    distances[starting] = 0 #starting vertex의 cost는 0
+
+    pq = [(0, starting)] # priority queue, 탐색할 노드 보관
+
+    # pq 안에 값이 사라질 때까지 반복
+    while len(pq) > 0:
+        current_distance, current_vertex = heapq.heappop(pq) #pop된 노드가 (distance, vertex) 형식 .
+        
+        if current_distance > distances[current_vertex]:
+            continue
+            
+        for neighbor, weight in graph[current_vertex].items():
+            distance = current_distance+weight
+            
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+                heapq.heappush(pq, (distance, neighbor))
+
+
+    return distances
+    
