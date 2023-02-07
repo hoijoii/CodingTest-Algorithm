70095f03c36498b12ee993d34e6e3b67b87e6f77
hoijoii <oodh3328@gmail.com>
2023-02-07 11:22:32 +0900

    비용테이블 고민

--- Dijkstra/baekjoon_1753_shortest_path.py
+++ Dijkstra/baekjoon_1753_shortest_path.py
@@ -4,14 +4,21 @@ ve = list(map(int, input().split())) # V, E
 start = int(input())
 uvw = [list(map(int, input().split())) for _ in range(ve[1])] # u, v, w
 
-""" costs = [[INF for _ in range(ve[0])] for _ in range(ve[0])] # 비용 테이블 (행:출발노드, 열:도착노드, 값:비용)
-for u, v, w in uvw:
-    costs[u-1][v-1] = w """
+# 비용테이블 만들기
+costs = [[float('inf') for _ in range(ve[0])] for _ in range(ve[0])] # 비용 테이블 (행:출발노드, 열:도착노드, 값:비용)
 
-"""priority queue 힙 구조 사용할 것"""
+for r in range(ve[0]):
+    costs[r][0] = 0
+
+for u, v, w in uvw:
+    costs[u-1][v-1] = w
+
+print(costs)
+
+# priority queue 힙 구조 dijkstra
 
 def dijkstra(graph, starting):
-    distances = {vertex: float('inf') for vertex in graph}
+    distances = {str(vertex+1): float('inf') for vertex in range(len(ve[0]))} # 최소비용 테이블
     distances[starting] = 0 #starting vertex의 cost는 0
 
     pq = [(0, starting)] # priority queue, 탐색할 노드 보관
@@ -20,6 +27,7 @@ def dijkstra(graph, starting):
     while len(pq) > 0:
         current_distance, current_vertex = heapq.heappop(pq) #pop된 노드가 (distance, vertex) 형식 .
         
+        #
         if current_distance > distances[current_vertex]:
             continue
             
@@ -32,7 +40,15 @@ def dijkstra(graph, starting):
 
     return distances
     
-graph = {}
+
+dijkstra(costs, start)
+""" graph = {}
 
 for i in range(ve[0]):
-    graph[str(i+1)]
+    graph[i] = {}
+    for value in uvw:
+        if value[0] == i :
+            graph[i].append({str(value[1]): value[2]})
+
+print(graph)
+     """
