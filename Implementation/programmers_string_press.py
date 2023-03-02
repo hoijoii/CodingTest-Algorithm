dad809de88b2ee123c8e16ba689eb21f88b4728c
hoijoii <oodh3328@gmail.com>
2023-03-02 18:00:32 +0900

    문자열압축

---
+++ Implementation/programmers_string_press.py
@@ -0,0 +1,14 @@
+# 문자열 끊는 단위는 한 번에 한 숫자만 가능
+# => 2단위로 끊고 뒤에는 3단위로 끊기 이런거 불가능
+
+def solution(s):
+
+    unit = 2
+
+    while(1):
+        lst = []
+        for letter in s:
+            if len(lst) > 2:
+                
+
+    return len(s)
