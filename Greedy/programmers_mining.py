d22d9dd66528d7ac6e4a6d059c701ed07f6fdaa3
hoijoii <oodh3328@gmail.com>
2023-06-15 14:54:55 +0900

    프로그래머스 광물캐기 정답

--- Greedy/programmers_mining.py
+++ Greedy/programmers_mining.py
@@ -7,7 +7,7 @@ picks: [dia, iron, stone]
 연속된 5개의 광물을 연속으로 캐야 함
 -> 연속된 5개를 하나의 그룹으로 생각하기
 -> 다이아/철/돌 사용했을 때 피로도를 그룹별로 리스트에 저장
--> 돌곡괭이 사용했을 때의 피로도를 기준으로 내림차순 정렬,
+-> 돌곡괭이 사용했을 때의 피로도를 기준으로 내림차순 정렬,(왜냐면 최악의 경우니까)
 -> 정렬된 순서대로 현재 갖고 있는 최선의 곡괭이를 사용했을 때 피로도를 정답에 더해주기
 
 https://velog.io/@seowj0710/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-Level-2-%EA%B4%91%EB%AC%BC-%EC%BA%90%EA%B8%B0-Java
@@ -15,9 +15,7 @@ https://velog.io/@seowj0710/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%
 
 def solution(picks, minerals):
     answer = 0
-    # 피로도
-    # 열: 광물
-    # 행: 곡괭이
+    # 피로도 표. 열: 광물 / 행: 곡괭이
     pirodo = [
         [1, 1, 1],
         [5, 1, 1],
@@ -25,31 +23,53 @@ def solution(picks, minerals):
     ]
 
     totalPicks = sum(picks)
-    pirodo_list = []
+    pirodo_list = [] # 5개씩 부술 때 곡괭이마다 드는 피로도 [dia, iron, stone]
 
+    #한 곡괭이로 5개 캐야 하니까 5개 아예 묶음
     for i in range(0, len(minerals), 5):
         if totalPicks == 0: break;
         
         dia, iron, stone = 0, 0, 0
+        # 5개 묶음 안에서 하나하나 곡괭이 피로도 더해서 리스트에 넣음
         for j in range(i, i+5):
             if j == len(minerals): break;
 
             val = 0
             if minerals[j] == "diamond": val=0
             elif minerals[j] == "iron" : val=1
-            else : val=3
+            elif minerals[j] == "stone" : val=2
 
             dia += pirodo[0][val]
             iron += pirodo[1][val]
             stone += pirodo[2][val]
 
-        pirodo_list.append([dia, iron, stone])
+        pirodo_list.append([dia, iron, stone]) # 5개 묶음을 부술 때 필요한 피로도 리스트에 추가
         totalPicks -= 1
 
-    print(pirodo_list)
-    #return answer
+    pirodo_list.sort(reverse=True, key=lambda x:x[2]) # 돌곡괭이 기준 내림차순
 
-solution([1, 3, 2], ["diamond", "diamond", "diamond", "iron", "iron", "diamond", "iron", "stone"])
+    # 피로도가 적게 드는 곡괭이로 많은 광물을 캘수록 좋음. => 내림차순해둔 리스트 돌면서 다이아 곡괭이부터 소진.
+    for p in pirodo_list:
+        dia = p[0]
+        iron = p[1]
+        stone = p[2]
+
+        if picks[0] > 0:
+            answer+=dia
+            picks[0] -= 1
+            continue
+        if picks[1] > 0:
+            answer+=iron
+            picks[1] -= 1
+            continue
+        if picks[2] > 0:
+            answer+=stone
+            picks[2] -= 1
+            continue
+
+    return answer
+
+""" solution([1, 3, 2], ["diamond", "diamond", "diamond", "iron", "iron", "diamond", "iron", "stone"]) """
 
 """
 
