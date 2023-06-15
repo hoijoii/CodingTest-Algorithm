1c6ba2980be15bf9b4e6ef8baf6020fabc9641e0
hoijoii <oodh3328@gmail.com>
2023-06-16 00:00:54 +0900

    프로그래머스 콜라츠 재귀 풀이

--- Implementation/programmers_collatz.py
+++ Implementation/programmers_collatz.py
@@ -1,17 +1,37 @@
 def collatz(n, cnt):
   if n > 1: 
-     if n%2 == 0 : n=n/2
-     else: n=n*3+1
-  return n
+    if n%2 == 0 : n=n/2
+    else: n=n*3+1
+    cnt+=1
+    return collatz(n, cnt)
+
+  elif n==1:
+    return cnt
 
 def solution(num):
     answer = 0
-    if num == 1: 
-      answer = 0
-    elif num != 1: 
-      count = 0
-      answer = collatz(num, count)
+    answer = collatz(num, answer)
+    if answer > 500:
+       answer = -1
 
-    print(collatz(num, count))
+    return answer
 
-solution(7)
+
+"""
+재귀 안 쓰고 풀기
+ 
+def solution(num):
+  answer=0 #count
+
+  while num != 1:
+    if num%2==0: 
+      num=num/2
+    else: 
+      num=num*3+1
+    
+    answer+=1
+    if answer > 500: return -1
+
+  return answer
+
+print(solution(6)) """
