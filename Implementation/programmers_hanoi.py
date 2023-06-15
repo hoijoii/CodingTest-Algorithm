6da6b3ad391fb31810bef405e3cb498b6975477c
hoijoii <oodh3328@gmail.com>
2023-06-16 00:01:17 +0900

    프로그래머스 하노이탑 정답

--- Implementation/programmers_hanoi.py
+++ Implementation/programmers_hanoi.py
@@ -1,6 +1,5 @@
 def move(answer, blockCnt, st, ed):
-    temp = 2 # temp는 st에서 ed로 가기 위한 중간과정 기둥
-    # 중간과정 기둥을 결정
+    temp = 2 # temp: 중간과정
     # range(1, 4): 1, 2, 3
     for i in range(1, 4):
         if i != st and i != ed:
@@ -9,11 +8,17 @@ def move(answer, blockCnt, st, ed):
     if blockCnt==1:
         answer.append([st, ed])
     else:
-        move(answer, blockCnt-1, st, temp) 
-        answer.append([st, ed]) # 맨 밑 블록을 3번기둥에 옮김
-        move(answer, blockCnt-1, temp, ed) # 나머지 블럭에 있던 걸 3번기둥에 옮기면 됨
+        move(answer, blockCnt-1, st, temp) # 일단 나머지 기둥에 옮김
+        answer.append([st, ed]) 
+        move(answer, blockCnt-1, temp, ed) 
 
 def solution(n):
     answer = []
     move(answer, n, 1, 3)
+
     return answer
+
+solution(3)
+
+
+# print('move('+str(answer)+','+str(blockCnt)+','+str(st)+','+ str(ed)+')')
