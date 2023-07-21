52ae7565de22759fbd68b4184cd569632eeb77a1
hoijoii <oodh3328@gmail.com>
2023-07-21 22:34:05 +0900

    프로그래머스 모음사전 정답

--- programmers_vowel_dict.py
+++ programmers_vowel_dict.py
@@ -3,5 +3,15 @@
 """
 
 def solution(word):
-    answer = 0
+    answer = len(word)
+    vowels = "AEIOU"
+    diff = [781, 156, 31, 6, 1];
+
+    for i in range(len(word)):
+        idx = vowels.index(word[i])
+        answer += idx * diff[i]
+
     return answer
+
+
+
