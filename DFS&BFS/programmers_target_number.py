c506f8cec625518a132ee6ddb0293d854163fd04
hoijoii <oodh3328@gmail.com>
2023-07-21 13:48:02 +0900

    프로그래머스 타겟넘버 정답

--- DFS&BFS/programmers_target_number.py
+++ DFS&BFS/programmers_target_number.py
@@ -4,17 +4,20 @@ https://school.programmers.co.kr/learn/courses/30/lessons/43165
 
 정수를 순서 안 바꾸고 더하거나 빼서 타겟 넘버 만들기
 방법의 수를 리턴하면 됨.
-dfs.. 모든 숫자를 거쳐야하고 모든 방법을 알아야 하니까
-
-stack
-
-트리
-왼쪽이 + 오른쪽이 -
+dfs->모든 숫자를 거쳐야하고 모든 방법을 알아야 하니까
 """
-
 def solution(numbers, target):
     answer = 0
 
+    def dfs(depth, totalSum):
+        nonlocal answer
+        if depth == len(numbers):
+            if totalSum == target: answer += 1
+            return 
     
+        dfs(depth+1, totalSum+numbers[depth])
+        dfs(depth+1, totalSum-numbers[depth])
+    
+    dfs(0, 0)
 
     return answer
