4634af006c0b8a99112cf3fd07aecb5f2dc01356
hoijoii <oodh3328@gmail.com>
2023-08-02 21:54:25 +0900

    프로그래머스 달리기경주 정답

--- Implementation/programmers_running.py
+++ Implementation/programmers_running.py
@@ -1,45 +1,19 @@
 """
-추월했을 때 이름이 불림.
-players: 1등부터 순서대로 담긴 배열
-
-.index()는 O(N)임 .. 쓰지말기
-
-answer에 이름을 집어넣는데
-만약 그 이름이 callings 안에 없으면 그대로 append,
-이름이 있으면 몇 개인지 세고 인덱스 그만큼 앞에서 append ?
-
-
-def solution(players, callings):
-    answer = []
-    rank = { player : idx for idx, player in enumerate(players) }
-
-    def getKey(val):
-        for key, value in rank.items():
-            if val == value:
-                return key
-
-    for c in callings:
-        rank[getKey(rank[c]-1)] += 1
-        rank[c] -= 1
-
-    answer = sorted(rank, key = lambda x : rank[x])
-
-    return answer
+callings에서 가져온 플레이어를 찾을 때 players 전체 검색 안하도록
+딕셔너리에서 바로 찾아옴
 """
 
 def solution(players, callings):
-    answer = ['' for _ in range(len(players))]
+    # 플레이어 등수 기록
     rank = { player : idx for idx, player in enumerate(players) }
 
     for c in callings:
-        rank[c] -= 1
-        
+        ahead = rank[c]-1 
+        behind = rank[c] 
+        # 기록된 등수 변경
+        rank[players[ahead]], rank[players[behind]] = behind, ahead
+        # 실제 순서 변경
+        players[ahead], players[behind] = players[behind], players[ahead]
 
-    for key, value in rank.items():
-        answer[value] = key
+    return players
 
-    print(answer)
-
-    return answer
-
-solution(["mumu", "soe", "poe", "kai", "mine"], ["kai", "kai", "mine", "mine"])
