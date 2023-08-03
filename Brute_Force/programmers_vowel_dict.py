22711224189cb89fced1162b0297199898a06128
hoijoii <oodh3328@gmail.com>
2023-08-03 16:26:03 +0900

    모음사전 폴더이동

---
+++ Brute_Force/programmers_vowel_dict.py
@@ -0,0 +1,13 @@
+def solution(word):
+    answer = len(word)
+    vowels = "AEIOU"
+    diff = [781, 156, 31, 6, 1];
+
+    for i in range(len(word)):
+        idx = vowels.index(word[i])
+        answer += idx * diff[i]
+
+    return answer
+
+
+
