10cfe329ad6dc062287fc64b006818111e3d78ca
hoijoii <oodh3328@gmail.com>
2023-09-05 20:53:41 +0900

    프로그래머스 의상 정답

--- Sort/programmers_clothes.py
+++ Sort/programmers_clothes.py
@@ -2,25 +2,21 @@
 의상
 https://school.programmers.co.kr/learn/courses/30/lessons/42578
 
-["의상", "신체부위"]
-가능한 조합 개수 return
+result=(headgear수+1)*(eyewear수+1)-1
+headgear 2가지 중 1개를 고를 수도 있고 하나도 안 고를 수 있음: 2+1
+eyewear 1가지 중 1개 고를 수도, 안 고를 수도: 1+1
+
 """
 
-def combination(closet, n):
-    for cloth in closet:
-        
 
 def solution(clothes):
-    answer = 0
+    answer = 1
 
-    closet = { part: 0 for cloth, part in clothes }
+    closet = { part: 0 for name, part in clothes }
 
-    for cloth in clothes:
-        closet[cloth[1]] += 1
+    for name, part in clothes:
+        closet[part] += 1
+    for n in closet.values():
+        answer *= (n+1) # (n개 중 한 개를 고르는 경우 + 하나도 안 고르는 경우)
 
-    for i in range(1, len(closet.items())+1):
-        combination(closet, i)
-
-    return answer
-
-solution([["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]])
+    return answer-1 # 전부 다 안 골랐을 경우 제외
