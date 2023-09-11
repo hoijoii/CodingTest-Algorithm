0b20ba904bd1638dc8dbc551a255060578edcbbd
hoijoii <oodh3328@gmail.com>
2023-09-11 16:11:06 +0900

    프로그래머스 호텔 방배정 주석 수정

--- Implementation/programmers_hotel_room.py
+++ Implementation/programmers_hotel_room.py
@@ -6,10 +6,10 @@ import sys
 sys.setrecursionlimit(10 ** 6)
 
 def assign(k, r, answer):
-    """ if r+1<k and r+1 not in answer:
+    if r+1<k and r+1 not in answer:
         answer.append(r+1)
     else:
-        assign(k, r+1, answer) """
+        assign(k, r+1, answer)
 
 def solution(k, room_number):
     answer = []
