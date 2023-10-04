e08b1251e4c68b8c8abfc606d7c7a3c6b6722cc4
hoijoii <oodh3328@gmail.com>
2023-10-04 14:44:27 +0900

    백준 1520  내리막길 정답

--- Dynamic_programming/baekjoon_1520_downhill.py
+++ Dynamic_programming/baekjoon_1520_downhill.py
@@ -5,61 +5,32 @@ https://www.acmicpc.net/problem/1520
 제일 왼쪽 위 지점에서 출발하여 제일 오른쪽 아래 지점까지 
 항상 내리막길로만 이동하는 경로의 개수 print
 
-오른쪽 아래까지 가기 위해 반복하는 걸 반복해야 함
+오른쪽 아래까지 가기 위해 반복하는 걸 반복해야 함 => dfs
 더이상 갈 길이 없을 때 반복이 끝나기.
-
-갈림길의 수를 그 위치에 숫자로 표시
-or
-bfs처럼..? 재귀로 들어가기 
-or
-dfs: 루트를 몇 번 더 반복해야 하니 시간 초과 부담,,
+메모이제이션 사용
 """
 M, N = map(int, input().split())
-graph = [[map(int, input().split())] for _ in range(M)]
-route = [[-1 for _ in range(N)] for _ in range(M)]
-count = 0
+graph = [list(map(int, input().split())) for _ in range(M)]
+dp = [[-1 for _ in range(N)] for _ in range(M)] # 메모이제이션
 
-direction = [[-1, 0], [1, 0], [0, -1], [0, 1]] 
+direction = [[-1, 0], [1, 0], [0, -1], [0, 1]]
 
 def dfs(y, x):
-  global count
-  
-  if y==M and x==N:
-    count+=1
-    return
-  
-  if route[y][x] == 0:
+  #목적지에 도달하면 메모에 1 더하도록.
+  #dp 상의 목적지를 -1로 냅두는 것이 중요..
+  if y==M-1 and x==N-1: 
+    return 1
+
+  if dp[y][x] == -1:
+    dp[y][x] = 0 #방문표시
+
     for dy, dx in direction:
       ny = y+dy
       nx = x+dx
-      if 0 <= ny < M and 0 <= nx < N and graph[y][x] > graph[ny][nx]:
-        
 
-""" def move(x, y):
-  global count
-  print(y, x)
-  for dy, dx in direction:
-    ny = y+dy
-    nx = x+dx
-
-    if y == M-1 and x == N-1:
-      count+=1
-      print('============================')
-      continue
-    
-    if 0 <= ny < M and 0 <= nx < N:
-      if graph[y][x] > graph[ny][nx]: 
-        move(nx, ny)
-      else:
-        continue
-
-move(0, 0)
+      if 0 <= ny < M and 0 <= nx < N and graph[ny][nx] != 0 and graph[y][x] > graph[ny][nx]:
+        dp[y][x] += dfs(ny, nx)
   
-print(count) """
+  return dp[y][x]
 
-
-
-""" for r in range(M):
-    for c in range(N):
-      move(c, r) """
-    
+print(dfs(0,0))
