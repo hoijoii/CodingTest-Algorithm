40a5b9686622080729e27e0b61510bafca3dd29b
hoijoii <oodh3328@gmail.com>
2023-10-05 18:03:48 +0900

    백준 환상의듀엣 점화식세우기

--- Dynamic_programming/baekjoon_11570_duet.py
+++ Dynamic_programming/baekjoon_11570_duet.py
@@ -2,77 +2,35 @@
 환상의 듀엣
 https://www.acmicpc.net/problem/11570
 
-차가 적은 값들을 묶어야 함 - 두묶음으로
-어떻게~해야하나,,
-전부 계산..?
-정렬 후 차가 작은 것들끼리 묶기. 왜냐면 작은 수랑 큰 수는 항상 차가 크니까
+abs(현재음-직전에부른음)이 최소화돼야함.
+모든 경우를 다 봐야 하니까 dp 사용하기
+dp[i][j]: i번째는 상덕, j번째는 희원이 마지막으로 불렀을 때 가질 수 있는 최소값
 
-노래를 부를 때 음의 순서가 바뀌면 안 됨 .. => 정렬 x
-pivot을 정하고 그거보다 큰 숫자들은 차례로 연산?
-
-정렬해서 pivot 찾고 music에서 해당하는 원소들 차례로 연산
-
-상덕이는 작은거부터
-희원이는 큰거부터 계산?
+max(i, j) : i, j 중 마지막으로 불려진 음
+희원: dp[i][j] + abs(music[j]-music[max(i, j)])
 """
 N = int(input())
 music = list(map(int, input().split()))
+dp = [[float('inf') for _ in range(N)] for _ in range(N)]
+answer = float('inf')
 
-diffs = []
-sang, hee = [], []
-answer = 0
+dp[0][1], dp[1][0] = 0, 0
 
-for i in range(len(music)):
-  if abs(max(music)-music[i]) > abs(min(music)-music[i]):
-    sang.append(music[i])
-  elif abs(max(music)-music[i]) < abs(min(music)-music[i]):
-    hee.append(music[i])  
+for i in range(N):
+  for j in range(N):
+    if i == j: continue
+    #max(i,j): 직전에 부른 음
+    dp[i][max(i,j)] = dp[i][j] + abs(music[i]-music[max(i, j)])
+    dp[max(i,j)][j] = dp[i][j] + abs(music[j]-music[max(i, j)])
 
-for i in range(len(sang)-1):
-  answer += abs(sang[i]-sang[i+1])
-
-for i in range(len(hee)-1):
-  answer += abs(hee[i]-hee[i+1])
+for i in range(N):
+  answer = min(answer, dp[i][N-1])
+  answer = min(answer, dp[N-1][i])
 
 print(answer)
 
-""" sorted_music = sorted(music)
-
-for i in range(len(music)-1):
-  #print(abs(sorted_music[i]-sorted_music[i+1]))
-  diffs.append(abs(sorted_music[i]-sorted_music[i+1]))
-
-pivot = sorted_music[diffs.index(max(diffs))+1]
-
-for i in range(len(music)):
-  if music[i] < pivot:
-    sang.append(music[i])
-  else:
-    hee.append(music[i])
-
-for i in range(len(sang)-1):
-  answer += abs(sang[i]-sang[i+1])
-
-for i in range(len(hee)-1):
-  answer += abs(hee[i]-hee[i+1])
-
-print(answer) """
-
-""" music.sort()
-print(music)
-for i in range(len(music)-1):
-  diffs.append(abs(music[i]-music[i+1]))
-
-pivot = diffs.index(max(diffs))+1
-
-for i in range(pivot-1):
-  answer += abs(music[i]-music[i+1])
-  print(abs(music[i]-music[i+1]))
-  
-print('----')
-
-for i in range(pivot, len(music)-1):
-  answer += abs(music[i]-music[i+1])
-  print(abs(music[i]-music[i+1]))
-
-print(answer) """
+"""
+5
+1 3 8 12 13
+7
+"""
