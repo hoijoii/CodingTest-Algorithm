fd8c9156c8b07c056b6fd9bba7c55180422c17bc
hoijoii <oodh3328@gmail.com>
2023-12-07 23:09:17 +0900

    백준 환상의 듀엣 정답

--- Dynamic_programming/baekjoon_11570_duet.py
+++ Dynamic_programming/baekjoon_11570_duet.py
@@ -10,8 +10,8 @@ max(i, j) : i, j 중 마지막으로 불려진 음
 희원: dp[i][j] + abs(music[j]-music[max(i, j)])
 """
 N = int(input())
-music = list(map(int, input().split()))
-dp = [[float('inf') for _ in range(N+1)] for _ in range(N+1)]
+music = [0] + list(map(int, input().split())) #0은 노래 시작 전 초기상태. 노래는 인덱스1부터
+dp = [[float('inf') for _ in range(N+1)] for _ in range(N+1)] #둘 중 맨처음 부르는 경우의 수(dp[0][i]랑 dp[i][0])도 고려해야 해서 N+1
 answer = float('inf')
 
 dp[0][1], dp[1][0] = 0, 0
@@ -21,25 +21,15 @@ for a in range(N+1):
     if a==b: continue
 
     nxt = max(a, b) + 1 #마지막 부른 음의 다음 음
-    if nxt >= N: continue
+    if nxt > N: continue
 
-    """ if a==0 or b==0:
-      music[0] = music[nxt] # 0이면 음이 바뀌는 cost가 없음. abs를 0으로 만들기 """
+    diff = 0 if a==0 else abs(music[a]-music[nxt]) # a가 아예 노래를 안 불렀던 경우 차를 구할 필요 없으니 0
+    dp[nxt][b] = min(dp[nxt][b], dp[a][b] + diff) # a가 다음 음을 부르는 경우
 
-    dp[nxt][b] = min(dp[nxt][b], dp[a][b] + abs(music[a]-music[nxt]))
-    dp[a][nxt] = min(dp[a][nxt], dp[a][b] + abs(music[b]-music[nxt]))
-    print(a, b, nxt, dp[nxt][b], dp[a][nxt])
-  print('===============================')
-  print(dp)
+    diff = 0 if b==0 else abs(music[b]-music[nxt]) # b가 아예 노래를 안 불렀던 경우 차를 구할 필요 없으니 0
+    dp[a][nxt] = min(dp[a][nxt], dp[a][b] + diff) # b가 다음 음을 부르는 경우
 
 for i in range(N):
-  answer = min(answer, dp[i][N-1])
-  answer = min(answer, dp[N-1][i])
+  answer = min(answer, dp[i][N]) 
 
 print(answer)
-
-"""
-5
-1 3 8 12 13
-7
-"""
