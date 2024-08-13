5b08a5a2396dbfe1ebe04c68773bd2724fe3c710
hoijoii <oodh3328@gmail.com>
2024-08-13 16:01:04 +0900

    백준 회사문화1 수정

--- Tree/baekjoon_14267_company_culture.py
+++ Tree/baekjoon_14267_company_culture.py
@@ -20,39 +20,46 @@ N, M = map(int, input().split()) # 회사 직원 수(직원은 1~N번 매겨져 
 boss_list = list(map(int, input().split())) # 직원들 각각의 상사
 compliment = [list(map(int, input().split())) for _ in range(M)] # 칭찬받은 직원의 칭찬 수치
 
-# 한 노드 = 직원
+# 노드 = 직원
 class Node:
-  def __init__(self, num, compli_num):
+  def __init__(self, num):
     self.num = num # 사번
-    self.compli_num = compli_num # 받은 칭찬 수치
+    self.compli_num = 0 # 받은 칭찬 수치
     self.children = [] # 여러 부하를 둘 수 있음
 
   def add_child(self, child):
     self.children.append(child)
 
+  def add_compliment(self, num):
+    self.compli_num += num
+
 # 전위 순회
 def preorder(node, parent_compli):
   if node:
-    node.compli_num += parent_compli # 노드 자신의 칭찬 수치에 부모 칭찬수치를 더함
+    node.add_compliment(parent_compli) # 노드 자신의 칭찬 수치에 부모 칭찬수치를 더함
 
     if node.children:
       for child in node.children:
-        preorder(tree[child], node.compli_num)
+        preorder(tree[child.num], node.compli_num)
 
-# 노드 먼저 만들기
-tree = { i: Node(i, 0) for i in range(1, N+1) }
+# 트리 만들기
+tree = { i: Node(i) for i in range(1, N+1) }
 root = tree[1]
 
 # 자식 노드 추가
-for child, boss in enumerate(boss_list):
-  if(boss == -1): continue
-  tree[boss].add_child(tree[child])
+# idx+1 = 사번
+for idx, boss in enumerate(boss_list):
+  if(boss == -1): 
+    continue
+  tree[boss].add_child(tree[idx+1])
 
-# 직속 상사에게 받은 칭찬 수치 저장
+# 직속 상사에게 받은 칭찬 수치 더함
 for num, comp in compliment:
-  tree[num].compliment = comp
+  tree[num].compli_num += comp
 
 preorder(root, 0) # 순회하며 칭찬 수치 더하기
 
-for num, node in tree.items():
-  print(node.compli_num)
+for node in tree.values():
+  print(node.compli_num, end=' ')
+
+print('\n')
