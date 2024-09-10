23094d3929821793a807a935cdd3bda617e3e834
hoijoii <oodh3328@gmail.com>
2024-09-10 18:36:35 +0900

    백준 퇴사2 문제 수정

--- Dynamic_programming/baekjoon_15486_quit_company.py
+++ Dynamic_programming/baekjoon_15486_quit_company.py
@@ -9,6 +9,8 @@ P=금액
 하루에 할 수 있는 상담은 하나
 
 정답=얻을 수 있는 최대 수익
+T가 제일 큰 것부터 시도해봐야 빨리 찾는 거 아님?
+조건: N-idx >= T 남은 날짜 수보다 T가 같거나 작아야 상담이 가능함
 
 7
 3 10
@@ -20,10 +22,36 @@ P=금액
 2 200
 
 45
+점화식 ,,
+dp[i + T] = max(dp[i + T], dp[i] + P)
 """
 N = int(input())
 schedule = [list(map(int, input().split())) for _ in range(N)]
-dp = [[-1]*N for _ in range(N)]
+dp = {}
 
-print(dp)
+for idx, item in enumerate(schedule):
+  t, p = item
+  
 
+
+""" schedule_T = []
+schedule_P = []
+
+for i in range(N):
+  t, p = list(map(int, input().split()))
+  schedule_T.append(t)
+  schedule_P.append(p)
+
+dp = [0]*N
+min_t = 51
+max_p = 0
+
+print(schedule_T)
+print(schedule_P)
+
+for idx, val in enumerate(schedule_P):
+  if schedule_T[idx] <= N-idx:
+    
+ """
+  """ if val <= N-idx:
+    min_t = min(val, min_t) """
