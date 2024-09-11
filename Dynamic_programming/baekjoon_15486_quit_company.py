9abb44b064b56c05d66804a8ce9de6b58cdddf33
hoijoii <oodh3328@gmail.com>
2024-09-11 18:22:11 +0900

    백준 퇴사2 정답

--- Dynamic_programming/baekjoon_15486_quit_company.py
+++ Dynamic_programming/baekjoon_15486_quit_company.py
@@ -27,11 +27,22 @@ dp[i + T] = max(dp[i + T], dp[i] + P)
 """
 N = int(input())
 schedule = [list(map(int, input().split())) for _ in range(N)]
-dp = {}
+dp = [0] * (N+1) # (index)일까지 얻을 수 있는 최대 수익 기록
 
 for idx, item in enumerate(schedule):
   t, p = item
-  
+  dp[idx] = max(dp[idx-1], dp[idx]) # 지금까지 최대 수익을 정함(dp[idx-1]: 어제까지의 최대수익, dp[idx]: 0 혹은 점화식으로 계산해서 나온 수익)
+
+  # 상담을 정해진 날짜 안에 마칠 수 있는지
+  if idx + t <= N:
+    # 오늘부터 상담을 시작하고 나서 i+t일에 얻을 수 있는 수익 기록.
+    # dp[idx+t]: 기존에 점화식으로 계산되어 나온 놈 or 0
+    # dp[idx]+p: 지금까지의 최대 수익에 상담진행했을 때 수익을 더한 넘
+    dp[idx+t] = max(dp[idx+t], dp[idx]+p) 
+
+print(max(dp[N-1], dp[N])) # dp[N]이 비어있을 수 있으므로 둘 중에 큰 값 선택.
+
+
 
 
 """ schedule_T = []
@@ -53,5 +64,3 @@ for idx, val in enumerate(schedule_P):
   if schedule_T[idx] <= N-idx:
     
  """
-  """ if val <= N-idx:
-    min_t = min(val, min_t) """
